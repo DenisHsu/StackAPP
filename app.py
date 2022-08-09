c89c6235f33dfed736865b1e60196c864d7c943e
DenisHsu <event1250159@gmail.com>
2022-08-09 17:29:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = event.message.text
     #line_bot_api.reply_message(event.reply_token,message)
     try:
         if re.match("股票 ",message):
