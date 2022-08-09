6ac40d919523e9ef7d9b5da30bcaf0cba81c93e8
DenisHsu <event1250159@gmail.com>
2022-08-09 14:20:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -39,7 +39,57 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    #line_bot_api.reply_message(event.reply_token,message)
+    if "股票 " in message:
+        buttons_template_message = TemplateSendMessage(
+        alt_text = "股票資訊",
+        template=CarouselTemplate( 
+            columns=[ 
+                CarouselColumn( 
+                            thumbnail_image_url ="https://www.moneyweekly.com.tw/Photo/%E8%AA%AA%E8%A7%A3%E8%B2%A1%E7%B6%93%E5%A4%A7%E5%B0%8F%E4%BA%8B/202109281943_510054.jpg",
+                            title = message[3:] + " 股票資訊", 
+                            text ="請點選想查詢的股票資訊", 
+                            actions =[
+                                MessageAction( 
+                                    label= message[3:] + " 個股資訊",
+                                    text= "個股資訊 " + message[3:]),
+                                MessageAction( 
+                                    label= message[3:] + " 個股新聞",
+                                    text= "個股新聞 " + message[3:]),
+                            ]
+                        ),
+                        CarouselColumn( 
+                            thumbnail_image_url ="https://www.moneyweekly.com.tw/Photo/%E8%AA%AA%E8%A7%A3%E8%B2%A1%E7%B6%93%E5%A4%A7%E5%B0%8F%E4%BA%8B/202109281943_510054.jpg",
+                            title = message[3:] + " 股票資訊", 
+                            text ="請點選想查詢的股票資訊", 
+                            actions =[
+                                MessageAction( 
+                                    label= message[3:] + " 最新分鐘圖",
+                                    text= "最新分鐘圖 " + message[3:]), 
+                                MessageAction( 
+                                    label= message[3:] + " 日線圖",
+                                    text= "日線圖 " + message[3:]),  
+                            ]
+                        ),
+                        CarouselColumn( 
+                            thumbnail_image_url ="https://www.moneyweekly.com.tw/Photo/%E8%AA%AA%E8%A7%A3%E8%B2%A1%E7%B6%93%E5%A4%A7%E5%B0%8F%E4%BA%8B/202109281943_510054.jpg",
+                            title = message[3:] + " 股利資訊", 
+                            text ="請點選想查詢的股票資訊", 
+                            actions =[
+                                MessageAction( 
+                                    label= message[3:] + " 平均股利",
+                                    text= "平均股利 " + message[3:]),
+                                MessageAction( 
+                                    label= message[3:] + " 歷年股利",
+                                    text= "歷年股利 " + message[3:])
+                            ]
+                        ),                               
+                    ]
+                ) 
+            )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
